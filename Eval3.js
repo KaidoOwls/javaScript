ca8a702cbc9bf2cc79a4ca732f2d33ccf999e682
Kaido <seydina.diallo60@gmail.com>
2022-11-07 16:13:57 +0100

    Modif eval 3

--- Eval3.js
+++ Eval3.js
@@ -1,14 +1,17 @@
-N=parseInt(prompt("Entrez le numéro de la table de multiplication que vous souhaitez voir"))
-table(N);
 
-function table(n)
-{
-    for (i=0;i<11;i++)
-    {
-        T=n*i;
-        console.log(n+" x "+i+" = "+T);
-        document.write(n+"x"+i+" = "+T+"  /  ")
-    }
-    return T;
+
+
+let prenom = prompt("Prenom");
+let tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+
+let i = tab.indexOf(prenom);
+if (i > -1) {
+    tab.splice(i ,1);
+    tab.push(' ');
 }
+else {
+    alert("Le prenom saisit n'est pas dans le tableau,allez bisous Clément")
+}
+console.log(tab)
 
+	
